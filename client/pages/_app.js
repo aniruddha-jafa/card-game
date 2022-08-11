64429ba8de39e3f323e3884fe6aad13dee57b2de
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-08-11 10:17:10 +0530

    added purecss for global base styles

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,20 @@
+import Head from 'next/head'
+import 'purecss/build/pure-min.css'
+
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta charSet='UTF-8' />
+                <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1.0'
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
