6f112ca51383248028d8e7b75084bb909942c1ba
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-08-11 14:18:04 +0530

    socket.io serverside

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
+const { Server } = require('socket.io')
+
+// ---------------------------------------------
+
+const PORT = 4060
+const CLIENT_ORIGIN_URL = 'http://localhost:3000'
 
 app = express()
+
+const httpServer = require('http').createServer(app)
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: CLIENT_ORIGIN_URL,
+    },
+})
 
 // -- middleware
 app.use(express.urlencoded({ extended: true }))
@@ -10,18 +24,34 @@ app.use(express.json())
 
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: CLIENT_ORIGIN_URL,
     })
 )
 
 app.use(helmet())
+
+io.use((socket, next) => {
+    const username = socket.handshake.auth.username
+    if (!username) {
+        return next(new Error('invalid username'))
+    }
+    socket.username = username
+    next()
+})
+
+io.on('connection', (socket) => {
+    console.info('A user connected')
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+    socket.emit('message', 'Hello!')
+})
 
 // -- routes
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' })
 })
 
-const PORT = 4060
-app.listen(PORT, (req, res) => {
+httpServer.listen(PORT, (req, res) => {
     console.log(`Listening at http://localhost:${PORT}`)
 })
